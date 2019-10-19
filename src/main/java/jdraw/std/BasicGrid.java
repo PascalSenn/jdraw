7ab409a3f9f6c0356acb952d90186bc68137fa42
Pascal Senn <pascal.senn@gia.ch>
2019-10-19 14:22:07 +0200

    feat(Grid): add basic grid

---
+++ src/main/java/jdraw/std/BasicGrid.java
@@ -0,0 +1,51 @@
+package jdraw.std;
+
+import jdraw.framework.DrawGrid;
+
+import java.awt.*;
+
+public class BasicGrid implements DrawGrid {
+    public final int step = 50;
+    private boolean activated = false;
+
+    @Override
+    public Point constrainPoint(Point p) {
+        int yDelta = p.y % step;
+        int xDelta = p.x % step;
+        yDelta = yDelta > step / 2 ? step-yDelta : -yDelta;
+        xDelta = xDelta > step / 2 ? step-xDelta : -xDelta;
+
+        return new Point(p.x + xDelta, p.y + yDelta);
+    }
+
+    @Override
+    public int getStepX(boolean right) {
+        return step;
+    }
+
+    @Override
+    public int getStepY(boolean down) {
+        return step;
+    }
+
+    @Override
+    public void activate() {
+        activated = true;
+    }
+
+    @Override
+    public void deactivate() {
+        activated = false;
+
+    }
+
+    @Override
+    public void mouseDown() {
+
+    }
+
+    @Override
+    public void mouseUp() {
+
+    }
+}
